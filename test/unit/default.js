ae9a35d54be45f932b8d2e48d059c24a56aac787
Dr. Pete Markiewicz <pindiespace@gmail.com>
2014-12-03 09:17:49 -0800

    added before and after to karma

--- test/unit/default.js
+++ test/unit/default.js
@@ -1,8 +1,24 @@
+/** 
+ * Tutorial at
+ * @link http://code.tutsplus.com/tutorials/testing-your-javascript-with-jasmine--net-21229
+ */
+
 describe ('my test suite', function() {
+
+	beforeEach(function () {
+
+	});
     
     it('BASIC KARMA TEST', function () {
     var bob = 10;
     expect(bob).toBe(100);
+    }); //this should FAIL
+
+    it('WeVidHere Initialization', function () {
+
     });
     
+    afterEach(function () {
+
+    });
 });
