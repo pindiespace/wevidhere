3a9696f7a4c227951377fa270f5f55c23e4f29e4
pindiespace <pindiespace@gmail.com>
2014-12-08 17:35:39 -0800

    hyperlinks

--- public/js/ag-controllers.js
+++ public/js/ag-controllers.js
@@ -1,11 +1,16 @@
 var WeVidHereConfig = angular.module('WeVidHere', []);
 
 //@link http://scotch.io/tutorials/javascript/building-dynamic-angular-forms-with-ngrepeat-and-ngform
+//@link http://www.javacodegeeks.com/2014/03/single-page-application-with-angular-js-node-js-and-mongodb-mongojs-module.html
+//@link http://scotch.io/tutorials/javascript/creating-a-single-page-todo-app-with-node-and-angular
+//@link http://code.realcrowd.com/on-the-bleeding-edge-advanced-angularjs-form-validation/
+//@link http://lostechies.com/gabrielschenker/2014/05/13/angularjspart-16-forms/
 
 WeVidHereConfig.controller(
 	'WeVidHereConfig', ['$scope', '$http', 
 		function ($scope, $http) {
 
+			//get data from the database
 			$http({
 				method: "GET",
 				url: "data/users.json"
@@ -16,8 +21,16 @@ WeVidHereConfig.controller(
 
 			});
 
-			$scope.orderProp = 'name';
-
+			//compare data from db with one of our forms
+			$scope.doLogin = function() {
+        		if($scope.formData.users[2].email === $scope.formData.username && 
+        			$scope.formData.users[2].password=== $scope.formData.password) {
+          				alert("Welcome");
+        			}
+        			else {
+        				alert("Lame, username:" + $scope.formData.username + " password:" + $scope.formData.password);
+        			}
+			}
 		} 
 	] 
 ); 
